6d0e70bf4e160d0cbcb520582c98ccb499dd2650
Amadeusz Szymko <amadeusz.szymko.2@tier4.jp>
2025-10-25 13:48:50 +0900

    fix: arm64 target cross compilation
    Signed-off-by: Amadeusz Szymko <amadeusz.szymko.2@tier4.jp>

--- spconv/cmake/Packing.cmake
+++ spconv/cmake/Packing.cmake
@@ -25,15 +25,26 @@ set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
 # Detect architecture for cross-compilation
 if(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64|arm64")
     set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "arm64")
+    # Custom suffix for filename differentiation
+    if(ARM64_CUDA_TARGET STREQUAL "aarch64-linux")
+      set(ARCH_SUFFIX "arm64-jetson")
+    else()
+      set(ARCH_SUFFIX "arm64-sbsa")
+    endif()
 elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64|amd64")
     set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "amd64")
+    set(ARCH_SUFFIX "amd64")
 else()
     set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE ${CMAKE_SYSTEM_PROCESSOR})
+    set(ARCH_SUFFIX ${CMAKE_SYSTEM_PROCESSOR})
 endif()
+
+# Use ARCH_SUFFIX in package filename
+set(CPACK_PACKAGE_FILE_NAME "${CMAKE_PROJECT_NAME}_${PROJECT_VERSION}_${ARCH_SUFFIX}")
 
 # package name for deb. If set, then instead of some-application-0.9.2-Linux.deb
 # you'll get some-application_0.9.2_amd64.deb (note the underscores too)
-set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
+# set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
 # that is if you want every group to have its own package,
 # although the same will happen if this is not set (so it defaults to ONE_PER_GROUP)
 # and CPACK_DEB_COMPONENT_INSTALL is set to YES
